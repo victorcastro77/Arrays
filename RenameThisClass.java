c9307738f2c67a47e775f1b9cdc1e399971a916e
Victor Castro II <victor.castro@mrjaffesclass.com>
2022-09-27 11:06:13 -0700

    Array

--- RenameThisClass.java
+++ RenameThisClass.java
@@ -9,5 +9,15 @@ public class RenameThisClass
 {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        String[] strArray = {"Array","Bravo","Charlie","Delta","Echo"};
+        for (int i = 0; i < strArray.length; i++) {
+            System.out.println(strArray[i]);
+        }
+        for (String value : strArray) {
+            System.out.print(value);
+        }
+        for (String value : strArray) {
+            System.out.print(value + "-");
+        }
     }
 }
